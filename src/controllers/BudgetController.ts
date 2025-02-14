754c4361f090bcc549cd72853ce755e734b44dcf
Fidel <fidel050987@gmail.com>
2025-02-14 10:29:50 -0600

    Agregando routing restapi

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from "express"
+import { response, type Request, type Response } from "express"
 import Budget from "../models/Budget"
 
 export class BudgetController {
@@ -9,7 +9,7 @@ export class BudgetController {
                     ['createdAt', 'DESC']
                 ],
                 //TODO: Filtrar por el usuario autenticado
-                
+
             })
         } catch (error) {
             //console.log(error)
@@ -29,12 +29,55 @@ export class BudgetController {
     }
 
     static getById = async (req: Request, res: Response) => {
-        console.log('Desde /api/budgets')
+
+        try {
+            const {id} = req.params
+            const budget = await Budget.findByPk(id)
+            if(!budget) {
+                const error = new Error('Presupuesto no encontrado')
+                res.status(404).json({error: error.message})
+            }
+            res.json(budget)
+        } catch (error) {
+            //console.log(error)
+            res.status(500).json({error: 'Hubo un error'})    
+        }
+        
     }
+
     static updateById = async (req: Request, res: Response) => {
-        console.log('Desde /api/budgets')
+        try {
+            const {id} = req.params
+            const budget = await Budget.findByPk(id)
+            if(!budget) {
+                const error = new Error('Presupuesto no encontrado')
+                res.status(404).json({error: error.message})
+            }
+            //Escribir los cambios del body
+            await budget.update(req.body)
+            res.json('Presupuesto Actualizado correctamente')
+
+        } catch (error) {
+            //console.log(error)
+            res.status(500).json({error: 'Hubo un error'})    
+        }
     }
+    
     static deleteById = async (req: Request, res: Response) => {
-        console.log('Desde /api/budgets')
+        try {
+            const {id} = req.params
+            const budget = await Budget.findByPk(id)
+            if(!budget) {
+                const error = new Error('Presupuesto no encontrado')
+                res.status(404).json({error: error.message})
+            }
+            //Escribir los cambios del body
+            await budget.destroy()
+            res.json('Presupuesto Eliminado correctamente')
+
+        } catch (error) {
+            //console.log(error)
+            res.status(500).json({error: 'Hubo un error'})    
+        }
     }
 }
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { body } from 'express-validator'
+import { body, param } from 'express-validator'
 import { BudgetController } from '../controllers/BudgetController'
 import { handleInputErrors } from '../middleware/validation'
 
@@ -20,10 +20,31 @@ router.post('/',
     BudgetController.create
 )
 
-router.get('/:id', BudgetController.getById)
+router.get('/:id',
+    param('id').isInt().withMessage('ID no válido')
+    .custom((value) => value > 0).withMessage('ID no válido'),
+    handleInputErrors,
+    BudgetController.getById
+)
 
-router.put('/:id', BudgetController.updateById)
+router.put('/:id', 
+    param('id').isInt().withMessage('ID no válido')
+    .custom((value) => value > 0).withMessage('ID no válido'),
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('amount')
+        .notEmpty().withMessage('La cantidad no puede ir vacia')
+        .isNumeric().withMessage('Cantidad no válida')
+        .custom((value) => value > 0).withMessage('Presupuesto debe ser mayor a 0'),
+        handleInputErrors,
+    BudgetController.updateById
+)
 
-router.put('/:id', BudgetController.deleteById)
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no válido')
+    .custom((value) => value > 0).withMessage('ID no válido'),
+    handleInputErrors,
+    BudgetController.deleteById
+)
 
 export default router
